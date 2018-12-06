c658ed19cb990888b908739b72150ae12ec25cf7
mynameiskate <katerina.akent@gmail.com>
2018-12-06 11:38:41 +0300

    updated radiogroup component change event

--- src/app/library/radiogroup/radiogroup.component.ts
+++ src/app/library/radiogroup/radiogroup.component.ts
@@ -11,6 +11,7 @@ import {
 import { ControlValueAccessorBase, CreateAccessorProvider } from '../../shared/control-value-accessor/ControlValueAccessorBase';
 import { RadiobuttonComponent } from '../radiobutton/radiobutton.component';
 import { RadiogroupService } from './radiogroup.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'clb-radiogroup',
@@ -26,18 +27,29 @@ export class RadiogroupComponent extends ControlValueAccessorBase<string> implem
 
   @Output() selectionChange = new EventEmitter<string>();
 
+  private subscription: Subscription;
+
   constructor(private el: ElementRef,
               private btnGroupService: RadiogroupService) {
     super();
+
+    this.subscription = this.btnGroupService
+      .selectedId$
+      .subscribe(id => this.onSelectionChange(id));
   }
 
   ngOnInit() {
     this.btnGroupService.init(this.formControlName, this.optionTemplate);
   }
 
-  onChange(target) {
-    this.writeValue(target.value);
-    this.onChangeCallback(target.value);
-    this.selectionChange.emit(target.value);
+  onSelectionChange(btnId: string) {
+    const item = this.radioButtons
+      .find(btn => btn.btnId === btnId);
+
+    if (item) {
+      this.writeValue(item.label);
+      this.onChangeCallback(item.label);
+      this.selectionChange.emit(item.label);
+    }
   }
 }
