31cf4c3204e98d4fc37363d857665a7101926dcc
mynameiskate <katerina.akent@gmail.com>
2018-12-18 11:44:00 +0300

    added placeholders to masked input

--- src/app/library/masked-input/masked-input.component.ts
+++ src/app/library/masked-input/masked-input.component.ts
@@ -1,4 +1,15 @@
-import { Component, ElementRef, ViewChild, OnInit, Input, Injector, Output, EventEmitter, HostListener } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  OnInit,
+  Input,
+  Injector,
+  Output,
+  EventEmitter,
+  HostListener,
+  AfterContentInit
+} from '@angular/core';
 import { NgControl } from '@angular/forms';
 import * as _ from 'lodash';
 import { ControlValueAccessorBase, CreateAccessorProvider } from '../../shared/control-value-accessor/ControlValueAccessorBase';
@@ -13,47 +24,55 @@ export const USMaskedInputTypes = {
   templateUrl: './masked-input.component.html',
   providers: [CreateAccessorProvider(MaskedInputComponent)]
 })
-export class MaskedInputComponent extends ControlValueAccessorBase<string> implements OnInit {
+export class MaskedInputComponent extends ControlValueAccessorBase<string> implements OnInit, AfterContentInit {
   @Input() inputLabel: string;
   @Input() inputType: string;
   @Input() mask: Array<string | RegExp> = USMaskedInputTypes.USPhone;
+  @Input() placeholder: string = '_';
 
   @Output() inputChange = new EventEmitter<string>();
 
   @ViewChild('element') element: ElementRef;
 
   control: NgControl;
+  cursorPosition: number = 0;
 
   @HostListener('paste', ['$event'])
-  onPaste(e: ClipboardEvent) {
-    e.preventDefault();
+  onPaste(event) {
+    event.preventDefault();
 
-    const { clipboardData } = e;
-    const newText = this.innerValue.slice(0, e.target.selectionStart)
+    const { clipboardData } = event;
+    const newText = this.innerValue
+      .slice(0, event.target.selectionStart)
       .concat(clipboardData.getData('Text'))
       .substring(0, this.mask.length);
 
-      for (let i = 0; i < newText.length; i++) {
-        if (_.isRegExp(this.mask[i])) {
-          const regexp = <RegExp>this.mask[i];
+    for (let i = 0; i < newText.length; i++) {
+      if (_.isRegExp(this.mask[i])) {
+        const regexp = <RegExp>this.mask[i];
 
-          if (!regexp.test(newText[i])) {
-            return false;
-          }
-        } else if (newText[i] !== this.mask[i]) {
+        if (!regexp.test(newText[i])) {
           return false;
         }
+      } else if (newText[i] !== this.mask[i]) {
+        return false;
       }
+    }
 
     this.writeValue(newText);
   }
 
-  constructor(private inj: Injector) {
+  constructor(private inj: Injector,
+              private el: ElementRef) {
     super();
   }
 
   ngOnInit() {
     this.control = this.inj.get(NgControl);
+  }
+
+  ngAfterContentInit() {
+    this.writeValue(this.maskedText);
   }
 
   writeValue(value: string) {
@@ -62,53 +81,72 @@ export class MaskedInputComponent extends ControlValueAccessorBase<string> imple
     this.inputChange.emit(value);
   }
 
-  updateValue() {
-    const value = this.element.nativeElement.value;
-    this.onChangeCallback(value);
+  setCursor() {
+    const input = this.el.nativeElement
+      .getElementsByTagName('input')[0];
+    input.focus();
+    input.setSelectionRange(this.cursorPosition,
+      this.cursorPosition, 'none');
   }
 
-  private getMaskStr(start: number, mask: Array<string | RegExp>): string {
-    let maskStr = '';
+  getMaskedText(start: number, end: number) {
+    let text = '';
 
-    while ((!_.isRegExp(mask[start])) && (start < mask.length)) {
-      maskStr += mask[start++];
-    }
+    this.mask.slice(start, end)
+      .map(item => (
+        text += _.isRegExp(item)
+          ? this.placeholder
+          : item
+      ));
 
-    return maskStr;
+    return text;
+  }
+
+  get maskedText() {
+    let text = '';
+
+    this.mask.map(item => (
+      text += _.isRegExp(item)
+        ? this.placeholder
+        : item
+    ));
+
+    return text;
   }
 
   insertText(event) {
     event.preventDefault();
 
     const { key, target } = event;
-    let { value } = event.target;
-    let index = value.length;
+    const { selectionEnd } = target;
+    let { value } = target;
+    let { selectionStart } = target;
 
     if ((key === 'Backspace') || (key === 'Delete')) {
-      const selectionLength = target.selectionEnd - target.selectionStart;
-      const delStart = _.findLastIndex(this.mask,
-        (exp, i) =>
-          (_.isRegExp(exp)) && (i < target.selectionStart + (selectionLength ? 1 : 0))
-      );
+      if (selectionEnd == selectionStart) {
+        selectionStart--;
+      }
 
-      value = value.slice(0, Math.max(delStart, 0));
-    } else if (value.length >= this.mask.length) {
-      return false;
-    } else {
-      value += this.getMaskStr(index, this.mask);
-      index = value.length;
-
-      const regexp = <RegExp>this.mask[index++];
+      const maskedText = this.getMaskedText(selectionStart , selectionEnd);
+      value = value.slice(0, selectionStart)
+        .concat(maskedText)
+        .concat(value.slice(selectionEnd));
+    } else if (_.isRegExp(this.mask[selectionStart])) {
+      const regexp = <RegExp>this.mask[selectionStart];
 
       if ((key.length == 1) && (regexp.test(key))) {
-        value += key;
-        value += this.getMaskStr(index, this.mask);
+        value = value.slice(0, selectionStart)
+          .concat(key)
+          .concat(value.slice(selectionEnd + 1));
       } else {
         return false;
       }
     }
 
+    this.cursorPosition = selectionEnd + 1;
     this.writeValue(value);
+    this.setCursor();
+
+    return false;
   }
 }
-
